e0add1cd98f33d8a671f018c17bb43ee69f423ca
Meruzhan <meruzhanmr91@mail.ru>
2023-03-07 14:06:00 +0300

    Task 1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write a number: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Write a number: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a*a == b)
+{
+    Console.WriteLine("Yes");
+}
+else{
+    Console.WriteLine("No");
+}

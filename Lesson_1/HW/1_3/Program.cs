e990bdedd9c8c6135d3b2ed7f6eb01ea3c5d3b34
Meruzhan <meruzhanmr91@mail.ru>
2023-03-09 17:46:10 +0300

    HW 1_3

---
+++ Lesson_1/HW/1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе
+//  показывает все чётные числа от 1 до N.
+Console.WriteLine("Write a number: ");
+int num = int.Parse(Console.ReadLine()!);
+int a = 2;
+int count = 0 ;
+while (num >= a )
+{
+    count = a;
+    a = a + 2;
+    Console.WriteLine(count);
+}
+

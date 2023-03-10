697c893557f598b383479ec92e8ed5dd74f39376
Meruzhan <meruzhanmr91@mail.ru>
2023-03-10 14:32:02 +0300

    Task 2_3

---
+++ Lesson_2/2_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// 3. Напишите программу, которая принимает на вход число и проверяет,
+//    кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+void del(int nm1)
+{
+    if (nm1 % 7 == 0 && nm1 % 23 == 0)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
+int a = new Random().Next(1,1000); 
+Console.WriteLine(a);
+del(a);

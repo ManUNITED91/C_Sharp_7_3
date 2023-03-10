6e85822c31237e26f9aff93931a8d9244e0be127
Meruzhan <meruzhanmr91@mail.ru>
2023-03-10 17:41:36 +0300

    Task 2_1 correct

--- Lesson_2/2_1/Program.cs
+++ Lesson_2/2_1/Program.cs
@@ -4,7 +4,7 @@ int Num(int num)
 {
     Console.WriteLine(num);
     int num1 = num / 10;
-    int num2 = num1 % 10;
+    int num2 = num % 10;
     if (num1 > num2) return num1;
     return num2;
 }

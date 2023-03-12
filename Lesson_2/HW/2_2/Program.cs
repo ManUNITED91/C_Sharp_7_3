43ce44a06ba2a734cf096fa6ef7144943e76ea8a
Meruzhan <meruzhanmr91@mail.ru>
2023-03-12 21:05:27 +0300

    HW 2_2

---
+++ Lesson_2/HW/2_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+
+Console.WriteLine("Write a number: ");
+int num = int.Parse(Console.ReadLine()!);
+int ThirdDig(int num1)
+{
+    while (num1 > 999)
+    {
+        num1 = num1 / 10;
+    }
+    return num1 % 10;
+}
+if (num > 99)
+{
+    int result = ThirdDig(num);
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Нет третьей цифры");
+}
+

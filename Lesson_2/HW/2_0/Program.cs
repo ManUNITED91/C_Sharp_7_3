f106875fd0599da2516e222052cd2d89bb1f4254
Meruzhan <meruzhanmr91@mail.ru>
2023-03-12 16:08:49 +0300

    HW 2_0

---
+++ Lesson_2/HW/2_0/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int SecDig(int digit)
+{
+   return digit / 10 % 10;
+}
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine()!);
+int result = SecDig(num);
+Console.WriteLine(result);

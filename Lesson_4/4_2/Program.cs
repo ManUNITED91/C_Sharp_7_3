12b4a9cc9838589f2a2225d012a07780f8ca8c5a
Meruzhan <meruzhanmr91@mail.ru>
2023-03-17 13:58:01 +0300

    Task 4_2

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2. Напишите программу, которая принимает на вход
+//    число N и выдаёт произведение чисел от 1 до N.
+int Times(int num)
+{
+    int sum = 1;
+    for (int i = 1;i <= num;i++)
+    {
+        sum = sum * i;
+    }
+    return sum;
+}
+Console.WriteLine("Enter a number: ");
+int a = int.Parse(Console.ReadLine()!);
+int result = Times(a);
+Console.WriteLine(result);

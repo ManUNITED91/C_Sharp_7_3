1f10f0b5561941bef01b683516c0faefe57d0e0c
Meruzhan <meruzhanmr91@mail.ru>
2023-03-17 12:56:16 +0300

    hw 3_3

---
+++ Lesson_3/HW/3_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+void Digit5(int num)
+{
+    if (num > 9999 && num < 100000)
+    {
+        int a = num % 10;
+        int b = num /10 %10;
+        int c = num / 1000;
+        int d = c % 10;
+        int e = c / 10;
+        Console.WriteLine($"{a},{b},{num / 100 % 10},{d},{e}");
+        if(a == e && b == d)
+        {
+            Console.WriteLine($"{num} -> palindrome");
+        }
+        else
+        {
+            Console.WriteLine($"{num} -> isn't palindrome");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{num} is not a 5-digit digit");
+    }
+}
+Console.WriteLine("Write a number: ");   
+int dig = int.Parse(Console.ReadLine()!);
+Digit5(dig);
+

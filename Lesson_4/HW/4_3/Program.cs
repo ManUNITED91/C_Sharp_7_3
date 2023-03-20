0417bad49b5c8cd3d532bdd189faaaddcfc26e41
Meruzhan <meruzhanmr91@mail.ru>
2023-03-20 15:28:13 +0300

    HW 4_3

---
+++ Lesson_4/HW/4_3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int[] CreateArr(int n, int firstNum, int lastNum)
+{
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new Random().Next(firstNum, lastNum);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+Console.WriteLine("Write the number of elements in the array");
+int lenght = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Write the range (from)");
+int dig1 = int.Parse(Console.ReadLine()!); 
+Console.WriteLine("Write the range (to)");
+int dig2 = int.Parse(Console.ReadLine()!);
+int[] res = CreateArr(lenght,dig1,dig2);
+
+PrintArr(res);
+
+

1a88294c3f93cbc177e707f2e280b3d21dcfd572
Meruzhan <meruzhanmr91@mail.ru>
2023-03-21 17:48:07 +0300

    Task 5_1 some changes

--- Lesson_5/5_1/Program.cs
+++ Lesson_5/5_1/Program.cs
@@ -1,45 +1,6 @@
 ﻿// 1. Напишите программу замена элементов массива:
 //    положительные элементы замените на соответствующие отрицательные, и наоборот.
-// void Print(int[] arr)
-// {
-//     int size = arr.Length;
 
-//     for (int i = 0; i < size; i++)
-//         Console.Write($"{arr[i]} ");
-//     Console.WriteLine();
-// }
-
-// int[] EightMass(int size, int from, int to)
-// {
-//     int[] arr = new int[size];
-
-//     for (int i = 0; i < size; i++)
-//         arr[i] = new Random().Next(from, to + 1);
-//     return arr;
-// }
-
-// void ChangeDig(int[] arr)
-// {
-    
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         if (arr[i] > 0)
-//             arr[i]*= -1 ;
-//         else if(arr[i]<0)
-//             arr[i]*= 1; 
-//         else
-//             arr[i] = 0; 
-//     }
-// }
-
-// int num = int.Parse(Console.ReadLine()!);
-// int start = int.Parse(Console.ReadLine()!);
-// int stop = int.Parse(Console.ReadLine()!);
-
-// int[] mass = EightMass(num, start, stop);
-// Print(mass);
-// ChangeDig(mass);
-// Print(mass);
 int[] RandArr()
 {
     Console.Write("Enter array length: ");

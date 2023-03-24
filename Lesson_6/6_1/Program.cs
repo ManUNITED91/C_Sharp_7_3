cd44d593114989b6ef33259be6ed7c378aea81eb
Meruzhan <meruzhanmr91@mail.ru>
2023-03-24 13:39:54 +0300

    task 6_1

---
+++ Lesson_6/6_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1. Напишите программу, которая принимает на вход три числа
+//    и проверяет, может ли существовать треугольник с сторонами такой длины.
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+void IsItTr(int num, int num1, int num2)
+{
+    if (num < num1 + num2 && num1 < num + num2 && num2 < num + num1)
+        Console.WriteLine("Yes");
+    else Console.WriteLine("No");
+}
+IsItTr(a,b,c);

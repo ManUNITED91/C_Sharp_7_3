c7bebafac9707a105e178c78fd8e2602acd7ec86
Meruzhan <meruzhanmr91@mail.ru>
2023-04-03 16:12:37 +0300

    HW 8_1

---
+++ Lesson_8/HW/8_1/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)        
+            Console.Write($" {arr[i, j]} ");        
+        Console.WriteLine();       
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)      
+        for (int j = 0; j < column; j++)        
+            arr[i, j] = new Random().Next(from, to + 1);                
+    
+    return arr;
+}
+
+void Descending(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1)-1; k++)
+            {
+                if(arr[i,k] < arr[i,k+1])
+                {
+                    (arr[i,k],arr[i,k+1]) = (arr[i,k],arr[i,k+1]);
+                }
+            }
+        }
+    }
+}
+Console.Write("Enter the number of rows: ");
+int rowNum = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int columnNum = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(rowNum, columnNum, start, stop);
+
+Print(mass);
+Descending(mass);
+
+Print(mass);
+

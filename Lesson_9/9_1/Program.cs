0a0da7aa5dbaa4e7cab5619e33d7260ffad0e1d6
Meruzhan <meruzhanmr91@mail.ru>
2023-04-04 13:42:03 +0300

    task 9_1

--- Lesson_9/9_1/Program.cs
+++ Lesson_9/9_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Задайте значение N. Напишите программу, которая выведет
+//    все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+Console.Write("Enter number N: ");
+int num = int.Parse(Console.ReadLine()!);
+
+void NaturalArr(int N)
+{
+    if(N == 0) return;
+    NaturalArr(N-1);
+    Console.Write($"{N} ");
+}
+NaturalArr(num);
